7b5dc8e325fe9a524f51d555fc9a7e12fe491e2f
Aameena Sana M <aameenasana180705@gmail.com>
2025-04-07 22:18:25 +0530

    added the login stuff

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <amin>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </amin>
+  );
+}
+
+export default App;
