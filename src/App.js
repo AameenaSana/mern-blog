b4849ab19db3f61842d79f583b152a35c656f7df
Aameena Sana M <aameenasana180705@gmail.com>
2025-04-11 00:14:40 +0530

    backend setup

--- src/App.js
+++ src/App.js
@@ -2,15 +2,42 @@ import './App.css';
 
 function App() {
   return (
-    <amin>
+    <main>
       <header>
         <a href="" className="logo">MyBlog</a>
         <nav>
           <a href="">Login</a>
           <a href="">Register</a>
         </nav>
-      </header>
-    </amin>
+      </header>      
+        <div className="post">
+          <div className="image">
+          <img src="https://i.pinimg.com/736x/4d/fa/bd/4dfabd569867dc502c522fba9550c6a6.jpg" alt=""/>
+          </div>
+          <div className="texts">
+          <h2>Hello Kitty</h2>
+          <p>Hello Kitty was created in 1974 and the first item, a vinyl coin purse, was introduced in 1975. Originally, Hello Kitty was only marketed towards pre-teenage girls, but beginning in the 1990s, the brand found commercial success among teenage and adult consumers as well. Hello Kitty's popularity also grew with the emergence of kawaii culture.</p>
+        </div>
+        </div>
+        <div className="post">
+        <div className="image">
+          <img src="https://i.pinimg.com/736x/4d/fa/bd/4dfabd569867dc502c522fba9550c6a6.jpg" alt=""/>
+          </div>
+          <div className="texts">
+          <h2>Hello Kitty</h2>
+          <p>Hello Kitty was created in 1974 and the first item, a vinyl coin purse, was introduced in 1975. Originally, Hello Kitty was only marketed towards pre-teenage girls, but beginning in the 1990s, the brand found commercial success among teenage and adult consumers as well. Hello Kitty's popularity also grew with the emergence of kawaii culture.</p>
+        </div>
+        </div>
+        <div className="post">
+        <div className="image">
+          <img src="https://i.pinimg.com/736x/4d/fa/bd/4dfabd569867dc502c522fba9550c6a6.jpg" alt=""/>
+          </div>
+          <div className="texts">
+          <h2>Hello Kitty</h2>
+          <p>Hello Kitty was created in 1974 and the first item, a vinyl coin purse, was introduced in 1975. Originally, Hello Kitty was only marketed towards pre-teenage girls, but beginning in the 1990s, the brand found commercial success among teenage and adult consumers as well. Hello Kitty's popularity also grew with the emergence of kawaii culture.</p>
+        </div>
+        </div>
+    </main>
   );
 }
 
