62815d39eebcfe39920d8ceb666903fe91d255a8
Aameena Sana M <aameenasana180705@gmail.com>
2025-04-14 21:20:41 +0530

    backend server working fine now

--- api/index.js
+++ api/index.js
@@ -1,12 +1,19 @@
-const express=require('express');
+const express= require('express');
 const cors=require('cors');
 const app=express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000', // Your React app's URL
+    methods: ['POST', 'OPTIONS']
+  }));
+app.use(express.json()); 
 
-app.post('/register',(req,res)=>{
-    res.json('test ok3');
-
-});
-
-app.listen(4000);
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    console.log('New user:', username);
+    res.json({ message: 'Registration successful!' });
+  });
+ 
+  app.listen(4000, () => {
+    console.log('Server running on http://localhost:4000');
+  });
